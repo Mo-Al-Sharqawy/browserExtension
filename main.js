97f22a810603ec58bf319b2efe009730fdded2b0
Mo-Al-Sharqawy <98462107+Mo-Al-Sharqawy@users.noreply.github.com>
2022-02-13 01:37:43 +0200

    second commit

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-let inputEl = document.getElementById("input-el");
-let buttonEl = document.getElementById("button-el");
-let ulEl = document.getElementById("ul-el");
+const inputEl = document.getElementById("input-el");
+const buttonEl = document.getElementById("button-el");
+const ulEl = document.getElementById("ul-el");
 let myArray = [];
+let oldArray = [];
+const deleteBtn = document.getElementById("delete-btn");
+const getDataFromArray = JSON.parse(localStorage.getItem("myArray"));
 
-buttonEl.addEventListener("click", function () {
-  myArray.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myArray", JSON.stringify(myArray));
-  console.log(localStorage.getItem("myArray"));
-  renderElement();
-});
-
-function renderElement() {
+console.log(getDataFromArray);
+if (getDataFromArray) {
+  myArray = getDataFromArray;
+  render(myArray);
+}
+function render(lead) {
   let x = "";
-  for (let i = 0; i < myArray.length; i++) {
-    x +=
-      "<li>" + "<a href='#'  target='_blank'>" + myArray[i] + "</a>" + "</li>";
+  for (let i = 0; i < lead.length; i++) {
+    x += `<li> <a href='#' target='_blank'>${lead[i]}</a></li>`;
   }
   ulEl.innerHTML = x;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myArray = [];
+  render(myArray);
+});
+// console.log(myArray);
+buttonEl.addEventListener("click", function () {
+  myArray.push(inputEl.value);
+  localStorage.setItem("myArray", JSON.stringify(myArray));
+  console.log(localStorage.getItem("myArray"));
+  render(myArray);
+});
